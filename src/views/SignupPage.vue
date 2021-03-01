478f8e5ec72a3a7ea07ea998efb2a67872d2f36d
lim ho <dlagh123@gmail.com>
2021-03-01 18:10:01 +0900

    .

---
+++ src/views/SignupPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>회원가입 페이지</h1>
+    <SignupFrom></SignupFrom>
+  </div>
+</template>
+
+<script>
+import SignupFrom from "../components/SignupFrom.vue";
+export default {
+  components: {
+    SignupFrom,
+  },
+};
+</script>
+
+<style>
+</style>
