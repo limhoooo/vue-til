118b989e9bd31908ee710284ed75172fc6c63545
lim ho <dlagh123@gmail.com>
2021-03-28 22:09:22 +0900

    .

--- src/api/common/interceptors.js
+++ src/api/common/interceptors.js
@@ -5,8 +5,6 @@ export function setInterceptors(instance) {
   instance.interceptors.request.use(
     function (config) {
       console.log("request");
-      // Do something before request is sent
-      // console.log(config);
       config.headers.Authorization = store.state.token;
       return config;
     },
@@ -20,13 +18,9 @@ export function setInterceptors(instance) {
   instance.interceptors.response.use(
     function (response) {
       console.log("response");
-      // Any status code that lie within the range of 2xx cause this function to trigger
-      // Do something with response data
       return response;
     },
     function (error) {
-      // Any status codes that falls outside the range of 2xx cause this function to trigger
-      // Do something with response error
       return Promise.reject(error);
     },
   );
